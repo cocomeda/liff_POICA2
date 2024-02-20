d75d4bc0457c4b03babc73dbcf73aee638a76c28
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-20 09:49:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -127,3 +127,24 @@ function textToUnicode(text) {
     return unicodeText;
 }
 	
+
+
+
+
+function getidToken(){
+        // LIFFの初期化
+        liff.init({ liffId: '2001269046-RZ90vdYB' }, () => {
+            if (liff.isLoggedIn()) {
+                // ユーザーがログインしている場合
+     
+                const idToken = liff.getIDToken(); // IDトークン
+		    
+            } else {
+                // ログインが必要な場合、ログインページを表示
+                liff.login();
+            }
+        });
+
+	return idToken;
+}
+
